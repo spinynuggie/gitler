e8db39670a8691a6a3d2697a3260a2f671fa5436
spinynuggie <rayantjedo@gmail.com>
2025-05-22 23:32:23 +0200

    Revert "Revert "Develop monster & saving HP""

--- src/main/java/org/example/AttackStrategy.java
+++ src/main/java/org/example/AttackStrategy.java
@@ -3,12 +3,10 @@ package org.example;
 public class AttackStrategy implements HinderingStrategy {
     @Override
     public void hinder(Player player, int strength) {
-        int hp = player.hp;
-        hp -= strength;
-        System.out.println("Monster attacks! Player loses " + strength + " HP.");
-
-        player.hp--;
+        // subtract exactly `strength` (never below 0)
+        player.hp = Math.max(player.hp - strength, 0);
+        System.out.println("Het monster valt aan! Je verliest "
+                + strength
+                + " HP. (Nu heb je nog " + player.hp + " HP)");
     }
 }
-
-
